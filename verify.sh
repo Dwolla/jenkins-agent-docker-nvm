90e1fa7d36f9762140b4cfc407050e93c5ab3865
Matt Travi <matt@dwolla.com>
2016-10-10 10:06:38 -0500

    switched $JENKINS_HOME for $NVM_DIR

--- verify.sh
+++ verify.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set +x
-echo running \`. ${JENKINS_HOME}/.nvm/nvm.sh --no-use\` > /dev/stderr
-. ${JENKINS_HOME}/.nvm/nvm.sh --no-use
+echo running \`. ${NVM_DIR}/nvm.sh --no-use\` > /dev/stderr
+. ${NVM_DIR}/nvm.sh --no-use
 
 echo running \`nvm install\` > /dev/stderr
 nvm install
