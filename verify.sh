dfafe9ced2bcb786e51526cb0245d5a4916f8d3d
Rocky Warren <rocky@dwolla.com>
2018-01-29 14:48:51 -0600

    Move common nvm install steps to separate script

--- verify.sh
+++ verify.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-set +x
-echo running \`. ${NVM_DIR}/nvm.sh --no-use\` > /dev/stderr
-. ${NVM_DIR}/nvm.sh --no-use
-
-echo running \`nvm install\` > /dev/stderr
-nvm install
-set -x
+./nvm-install.sh
 
 npm install
 npm test
